d9df260889e5012f38f3be0f548f48ea0be35a90
Kazel Deligero <kazel_deligero@yahoo.com>
2023-04-13 19:54:58 +0800

    feat: iex chart

--- Gemfile
+++ Gemfile
@@ -74,4 +74,4 @@ end
 gem "tailwindcss-rails", "~> 2.0"
 gem 'sendgrid-ruby'
 gem 'faraday'
-gem 'mail'
+gem 'mail'gem "rails_charts"
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,10 +1,13 @@
 class PortfoliosController < ApplicationController
 
+
   def index
     @portfolios = Portfolio.all
+    
   end
 
   def show
+    @chart_stock = get_chart_data
   end
 
   def new
@@ -29,7 +32,27 @@ class PortfoliosController < ApplicationController
   private
 
   def portfolio_params
+    params.require(:stock_symbol)
+  end
 
+  def get_symbol
+    @symbol = Portfolio.find(params[:stock_symbol])
+  end
+
+  def get_chart_data
+    @client = IEX::Api::Client.new(
+      publishable_token: 'pk_06f0670b09884fe5aa66d394e4263f00',
+      secret_token: 'sk_f528b0c334f24d498705a205d72a7ec4',
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+    @chart = @client.chart(params[:stock_symbol])
+
+    chart_arr = @chart.reduce([]) { |init, curr|
+      init.push([curr['label'], curr['open'], curr['close'], curr['high'], curr['low']]);
+    }.inject({}) do |res, k|
+      res[k[0]] = k[1..-1]
+    res
+    end
   end
 
 end
