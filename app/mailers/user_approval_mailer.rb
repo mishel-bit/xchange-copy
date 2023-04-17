698c348b759a472df0e7d8a8adc72b1b8f48a513
Michelle Santiago <mdcsantiago0@gmail.com>
2023-04-17 14:03:57 +0800

    refactor: user mailers into one class

--- app/mailers/user_approval_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserApprovalMailer < ApplicationMailer
-    default :from => 'xchange.stocks@zohomail.com'
-    
-    def send_approval_email(user)
-        @user = user
-        mail( to: @user.email,
-        subject: 'Xchange Account has been approved!' )
-    end
-end
--- app/mailers/user_pending_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserPendingMailer < ApplicationMailer
-    default :from => 'xchange.stocks@zohomail.com'
-    
-    def send_pending_email(user)
-        @user = user
-        mail( to: @user.email,
-        subject: 'Xchange Account Pending Approval' )
-    end
-end
