b2f7fbe0c5dd475ffee5f6fb3d9247c5974c9651
Kazel Deligero <kazel_deligero@yahoo.com>
2023-04-19 02:53:45 +0800

    build: add echart

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,5 @@ import 'flowbite';
 
 
 
+import "echarts"
+import "echarts/theme/dark"
--- config/importmap.rb
+++ config/importmap.rb
@@ -11,3 +11,5 @@ pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowb
 
 # pin "flowbite", to: "https://ga.jspm.io/npm:flowbite@1.6.4/lib/esm/index.js"
 # pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.7/lib/index.js"
+pin "echarts", to: "echarts.min.js"
+pin "echarts/theme/dark", to: "echarts/theme/dark.js"
