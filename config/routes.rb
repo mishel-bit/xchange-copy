5e0d577865ee466825dcefe749d018517bf4a76e
Kazel Deligero <kazel_deligero@yahoo.com>
2023-04-19 01:36:29 +0800

    feat: add via turbo

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,10 @@ Rails.application.routes.draw do
       resources :users
     end
 
+    resources :portfolios do
+      resources :transactions
+      end
+
   get '/portfolios' => 'portfolios#index'
   get '/portfolios/show' => 'portfolios#show', as: 'show_portfolio'
 
