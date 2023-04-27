c7219351482a1ee92f4ab31d6fbf28b9a8c7ec6c
Michelle Santiago <mdcsantiago0@gmail.com>
2023-04-28 02:45:04 +0800

    refactor: iex cloud

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     @latest_date = @stocks_active.last.latest_update_t
     @stocks_gainers = IEX_CLIENT.stock_market_list(:gainers).sort_by(&:change_percent).reverse
     @stocks_decliners = IEX_CLIENT.stock_market_list(:losers).sort_by(&:change_percent)
-    @stocks_cloud = Api::Iex::Cloud.call
+    @stocks_cloud = IexCloud.call
     @stocks_array = Kaminari.paginate_array(@stocks_cloud, total_count:@stocks_cloud.count).page(params[:page]).per(10)
   end
 
--- config/initializers/iex_cloud.rb
+++
@@ -1,5 +0,0 @@
-# require 'faraday'
-# require "ostruct"
-
-# response = Faraday.get('https://api.iex.cloud/v1/data/CORE/REF_DATA?token='+Rails.application.credentials.dig(:iex, :publishable_token))
-# IEX_CLOUD = JSON.parse(response.body, object_class: OpenStruct)
